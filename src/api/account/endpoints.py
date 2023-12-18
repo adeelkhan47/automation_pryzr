e7690799957eda5687e511f482fc6c38bbe8ed14
= <adiiiiikhan1998@outlook.com>
2023-12-18 23:34:51 +0500

    security

--- src/api/account/endpoints.py
+++ src/api/account/endpoints.py
@@ -1,33 +1,46 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from fastapi import Request
 from fastapi_sqlalchemy import db
 
 from api.account.schemas import GetAccounts
 from common.enums import Platforms
+from config import settings
+from helpers.deps import Auth, Admin_Auth
+from helpers.jwt import create_admin_access_token
 from model import Email, User
 from model.account import Account
 from model.account_user import AccountUser
 from model.user_emails import UserEmail
+from datetime import datetime,timedelta
+from typing import Optional
 
 router = APIRouter()
 
 
 @router.post("/get_accounts", response_model=GetAccounts)
-def get_accounts(request: Request, secret_key: str):
+def get_accounts(request: Request,found: bool = Depends(Admin_Auth())):
     accounts, count = Account.filter_and_order({})
     data = {"accounts": accounts, "count": count}
     return data
 
 
-@router.get("/get_accounts")
-def get_accounts(request: Request, account_unique_key: str):
+@router.get("/get_agents_stats")
+def get_accounts(request: Request, account_unique_key: str, start_date: Optional[str] = None,
+                 end_date: Optional[str] = None,found: bool = Depends(Admin_Auth())):
+    args = {}
+    if start_date and end_date:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        end_date = end_date + timedelta(days=1, minutes=-1)
+
+        args = {"created_at:gte": start_date, "created_at:lte": end_date}
+
     account = Account.get_by_unique_id(account_unique_key)
     if not account:
         raise HTTPException(status_code=404, detail="Not Found")
-    args = {}
-    data = {}
+
+    data = []
     for each in account.users:
-        print(each.user.email)
         inner_data = {}
         query = db.session.query(Email).filter(Email.status == "Successful")
         query = query.join(UserEmail, UserEmail.email_id == Email.id)
@@ -40,5 +53,17 @@ def get_accounts(request: Request, account_unique_key: str):
             inner_data[platform.value] = 0
         for email in emails:
             inner_data[email.platform] += int(email.amount)
-        data[each.user.email] = inner_data
+        data.append({each.user.email:inner_data})
     return data
+
+@router.get("/admin_secret")
+def admin_secret(secret_key, request: Request):
+    if secret_key == settings.admin_secret_key:
+        access, refresh = create_admin_access_token(secret_key)
+        token = {
+            "access_token": access
+        }
+        return token
+    else:
+        raise HTTPException(status_code=403, detail="Unauthorized.")
+
