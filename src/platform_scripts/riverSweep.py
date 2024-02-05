54b144143cfbd493196dc1958c1ed1be2675faac
= <adiiiiikhan1998@outlook.com>
2024-02-05 18:56:58 +0500

    26

--- src/platform_scripts/riverSweep.py
+++ src/platform_scripts/riverSweep.py
@@ -44,9 +44,7 @@ def run_script(userid, amount, username, password):
             while not found:
                 logging.error(count)
                 time.sleep(1)
-
-                record = wait.until(
-                    EC.presence_of_element_located((By.XPATH, f"//*[@id='select2-usersIds-results']/li[{count}]")))
+                record = wait.until(EC.presence_of_element_located((By.XPATH, f"//*[@id='select2-usersIds-results']/li[{count}]")))
                 logging.error(record.text.lower())
                 if record.text.lower() == userid.lower():
                     record.click()
